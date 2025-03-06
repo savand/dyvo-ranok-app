21535f5547a49f582b3e889f7e787bd36e932637
shelter <arturshelter@gmail.com>
2025-03-06 16:02:22 +0200

    update be url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://ec2-3-88-53-149.compute-1.amazonaws.com:8080/v1/form',
+  backendUrl: 'https://b.dyvo-ranok.com.ua/v1/form',
 };
