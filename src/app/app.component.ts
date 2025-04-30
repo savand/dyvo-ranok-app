b1708a3f3c3983f0f53e44160b058effb7e4fc0f
shelter <arturshelter@gmail.com>
2025-04-30 09:04:35 +0300

    fix debug point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { MmHeadComponent } from "./mm-head/mm-head.component";
-import { MmFooterComponent } from "./mm-footer/mm-footer.component";
-import { MmBodyComponent } from "./mm-body/mm-body.component";
+import { MmHeadComponent } from './mm-head/mm-head.component';
+import { MmFooterComponent } from './mm-footer/mm-footer.component';
+import { MmBodyComponent } from './mm-body/mm-body.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ MmHeadComponent, MmBodyComponent, MmFooterComponent],
+  imports: [MmHeadComponent, MmBodyComponent, MmFooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dyvo-ranok-app';
