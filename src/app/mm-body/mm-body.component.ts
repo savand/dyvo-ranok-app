f616adc558e9488e51a03108099999658466057e
shelter <arturshelter@gmail.com>
2025-05-02 13:02:11 +0300

    add about us

--- src/app/mm-body/mm-body.component.ts
+++ src/app/mm-body/mm-body.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SellItemComponent } from './sell-item/sell-item.component';
 import { OrderFormComponent } from './order-form/order-form.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @Component({
   selector: 'app-mm-body',
-  imports: [SellItemComponent, OrderFormComponent],
+  imports: [SellItemComponent, OrderFormComponent, AboutUsComponent],
   templateUrl: './mm-body.component.html',
   styleUrl: './mm-body.component.scss',
 })
